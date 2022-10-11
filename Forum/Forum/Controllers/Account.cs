62e698d2e865aed332c9c2f5ce767851bbe5c00b
doki42 <andras.szogi@gmail.com>
2022-10-11 13:12:02 +0200

    Implement main menu and dummy registration

---
+++ Forum/Forum/Controllers/Account.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Forum.Controllers;
+
+public class Account : Controller
+{
+    // GET
+    public IActionResult Register()
+    {
+        return View();
+    }
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
---
+++ Forum/Forum/Models/LoginViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Forum.Models;
+
+public class LoginViewModel
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public bool RememberMe { get; set; }
+    public string ReturnUrl { get; set; }
+}
