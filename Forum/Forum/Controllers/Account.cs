50ab74b72b362340f41efb4ab51e9b39c653ab89
doki42 <andras.szogi@gmail.com>
2022-10-13 11:38:26 +0200

    Fix connection to menubar

--- Forum/Forum/Controllers/Account.cs
+++ Forum/Forum/Controllers/Account.cs
@@ -1,18 +1,28 @@
-﻿using Forum.Models;
+﻿using Forum.Daos.Implementations;
+using Forum.Models;
+using Forum.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Forum.Controllers;
 
 public class Account : Controller
 {
+    public Account()
+    {
+        PostService = new PostService(PostDaoMemory.GetInstance(), TopicDaoMemory.GetInstance());
+    }
+
+    public PostService PostService { get; set; }
     // GET
     public IActionResult Register()
     {
+       ViewBag.topics = PostService.GetAllTopics();
         return View();
     }
 
     public IActionResult Login(string returnUrl = "")
     {
+        ViewBag.topics = PostService.GetAllTopics();
         var model = new LoginViewModel { ReturnUrl = returnUrl }; 
         return View(model);
     }
