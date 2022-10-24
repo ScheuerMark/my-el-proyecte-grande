c4b0f6d24991fe89efa5f38f0c9b800f0b76f063
Scheuer Mark <markscheuer2002@gmail.com>
2022-10-24 12:52:33 +0200

    Configure proxy and test

--- Forum/forum-frontend/src/App.js
+++ Forum/forum-frontend/src/App.js
@@ -1,13 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+
+  let d;
+  fetch('api/Test')
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {d}
         </p>
         <a
           className="App-link"
