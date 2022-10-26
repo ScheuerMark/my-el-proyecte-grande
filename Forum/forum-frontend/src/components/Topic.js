b661a5b7ef4b363e4fbf8b3607c486215ec983dc
Scheuer Mark <markscheuer2002@gmail.com>
2022-10-26 07:47:21 +0200

    Add icon to post count on topic

--- Forum/forum-frontend/src/components/Topic.js
+++ Forum/forum-frontend/src/components/Topic.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCommentAlt } from '@fortawesome/free-solid-svg-icons';
 
 export class Topic extends Component {
+   
+    constructor(props) { 
+      super(props); 
+      this.state = {
+          commentIcon: <FontAwesomeIcon icon={faCommentAlt} />,
+      }; 
+    } 
     render() {
       return (
         <div className="card">
             <div className="card-header">
                 <h5 className="w-75 d-inline-flex"><a className=" w-100 text-black text-decoration-none">{this.props.data.title}</a></h5>
-                <span className="float-end post-count fst-italic">{this.props.data.numberOfPosts}</span>
+                <span className="float-end post-count fst-italic">{this.props.data.numberOfPosts} {this.state.commentIcon}</span>
             </div>
             <div className="card-body row">
                 <div className="col-9">
