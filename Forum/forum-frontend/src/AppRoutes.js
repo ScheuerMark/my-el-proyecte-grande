1b9ecfd7e991eb84f1d23eea67ddb4caf9f482a8
Scheuer Mark <markscheuer2002@gmail.com>
2022-10-27 08:48:26 +0200

    Refactor routes to separate file

--- Forum/forum-frontend/src/AppRoutes.js
+++ Forum/forum-frontend/src/AppRoutes.js
@@ -1,12 +1,31 @@
-
-import { Topic } from "./components/Topic";
+import Home from "./components/Home";
+import PostDetails from "./components/PostDetails";
+import TopicDetails from "./components/TopicDetails";
+import Register from "./components/Register";
+import Login from "./components/Login";
 
 const AppRoutes = [
   {
-    path: '/Topic',
-    element: <Topic />
+    index: true,
+    element: <Home />
   },
+  {
+    path: '/PostDetail/:id',
+    element: <PostDetails />
+  },
+  {
+    path: '/Posts/:title',
+    element: <TopicDetails />
+  },
+  {
+    path: '/Register',
+    element: <Register />
+  },
+  {
+    path: '/Login',
+    element: <Login />
+  },
+  
 ];
 
 export default AppRoutes;
-
