ed7c79bc2c06a693dfd1328ff069d61423cc0d22
Attila Nagy <nagyattila86.hu@gmail.com>
2022-11-08 14:27:51 +0100

    Use funtcion component insted of class component for Home

--- Forum/forum-frontend/src/components/Home/Home.js
+++ Forum/forum-frontend/src/components/Home/Home.js
@@ -1,65 +1,39 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import './Home.css';
 import { Post } from '../Shared/Post';
 import Topic from '../Shared/Topic';
 
+function Home() {
+    let [topics, setTopics] = useState([]);
+    let [posts, setPosts] = useState([]);
 
-
-export class Home extends Component {   
-
-    constructor(props) { 
-        super(props); 
-        this.state = {
-            topics: [],
-            recentPosts: []
-        }; 
-    } 
-
-    componentDidMount() {
+    useEffect(()=>{
         fetch('api/Home/Topics')
         .then((response) => response.json())
         .then(data => {
-            this.setState({ topics: data });
-        });
+            setTopics(data);
+        })
         fetch('api/Home/Posts/Date/Desc')
         .then((response) => response.json())
         .then(data => {
-            this.setState({ recentPosts: data });
+            setPosts(data);
         });
-    }
+    }, [])
 
-    renderTopics(){
-        let topics = [];
-        for (let index = 0; index < this.state.topics.length; index++) {   
-            topics.push(<Topic key={this.state.topics[index].title} data={this.state.topics[index]} />);
-        }
-        return topics;
-    }
-
-    renderRecentPosts(){
-        let recentPosts = [];
-        for (let index = 0; index < this.state.recentPosts.length; index++) {   
-            recentPosts.push(<Post key={this.state.recentPosts[index].id} data={this.state.recentPosts[index]} />);
-        }
-        return recentPosts;
-    }
-
-    render() {
-      return (
-    <div>
-        <div className="row">
-            <div className="col-xl-6 col-lg-12 column1">
-                <h1>Topics</h1>
-                {this.renderTopics()}
-            </div>
-            <div className="col-xl-6 d-xl-block d-none">
-                <h1>Recent posts</h1>
-                {this.renderRecentPosts()}
-            </div>
-        </div>		
-    </div>
-      );
-    }
-  }
+    return (
+        <div>
+            <div className="row">
+                <div className="col-xl-6 col-lg-12 column1">
+                    <h1>Topics</h1>
+                    {topics.map((topic, index) => <Topic key={index} data={topic} />)}
+                </div>
+                <div className="col-xl-6 d-xl-block d-none">
+                    <h1>Recent posts</h1>
+                    {posts.map((post, index) => <Post key={index} data={post} />)}
+                </div>
+            </div>		
+        </div>
+    )
+}
   
-  export default Home;
+export default Home;
