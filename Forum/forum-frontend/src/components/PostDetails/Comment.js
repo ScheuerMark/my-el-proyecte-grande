44b9c1b47557dbf083e2a57c5d882fcd3f0c74a7
Attila Nagy <nagyattila86.hu@gmail.com>
2022-11-09 11:08:46 +0100

    Use only the comment prop insted of all props in Comment

--- Forum/forum-frontend/src/components/PostDetails/Comment.js
+++ Forum/forum-frontend/src/components/PostDetails/Comment.js
@@ -4,26 +4,26 @@ import { faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
 import { Link } from 'react-router-dom';
 
 
-export function Comment(props) {
+export function Comment({comment}) {
 
     const like = <FontAwesomeIcon icon={faThumbsUp} />;
     const dislike = <FontAwesomeIcon icon={faThumbsDown} />;
-    const date = new Date(props.data.dateTime).toLocaleString();
+    const date = new Date(comment.dateTime).toLocaleString();
 
 
     function likeComment(){
-        fetch(`/api/Home/Like/${props.data.id}`, {
+        fetch(`/api/Home/Like/${comment.id}`, {
             method: 'PUT',
         });
-        props.data.like++;
+        comment.like++;
     }
 
 
     function disLikeComment(){
-        fetch(`/api/Home/DisLike/${props.data.id}`, {
+        fetch(`/api/Home/DisLike/${comment.id}`, {
             method: 'PUT',
         });
-        props.data.disLike++;
+        comment.disLike++;
     }
 
 return (
@@ -34,12 +34,12 @@ return (
         </div>
     <div class="card-body row" >
         <div class="col-12">
-            <p class="card-text">{props.data.message}</p>
+            <p class="card-text">{comment.message}</p>
         </div>
         <div class="col-12 d-flex flex-column">
             <span class="fst-italic mt-auto ms-auto">
-                <Link onClick={likeComment} class="align-middle text-decoration-none text-black me-3">{like} {props.data.like}</Link>
-                <Link onClick={disLikeComment} class="align-middle text-decoration-none text-black">{dislike} {props.data.disLike} </Link>
+                <Link onClick={likeComment} class="align-middle text-decoration-none text-black me-3">{like} {comment.like}</Link>
+                <Link onClick={disLikeComment} class="align-middle text-decoration-none text-black">{dislike} {comment.disLike} </Link>
             </span>
         </div>
     </div>
