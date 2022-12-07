81d9ea32f870cdcb207b11834e31be2c537a06f9
doki42 <andras.szogi@gmail.com>
2022-12-07 20:48:24 +0100

    Add TopicTest

---
+++ Forum/ForumTest/TopicTest.cs
@@ -0,0 +1,23 @@
+﻿using Forum.Services;
+using NUnit.Framework;
+
+namespace ForumTest;
+
+[TestFixture]
+public class TopicTest : TestBase
+{
+    private TopicService _topicService;
+    
+    [SetUp]
+    public new void Setup()
+    {
+        base.Setup();
+        _topicService = new TopicService(_forumContext);
+    }
+
+    [Test]
+    public void TestGetAllTopics()
+    {
+        
+    }
+}
